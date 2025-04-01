f24c4a1fddc9707f37cc7bc39f2a9484414b61de
Smitayadav01 <960smitayadav@gmail.com>
2025-04-01 18:40:55 +0530

    Updated Edit Section

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const mysql=require("mysql2");
 const express=require("express");
 const app=express();
 const path=require("path");
+const methodOverride=require("method-override");
 
 const port=8080;
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
 app.set("view-engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
@@ -30,8 +33,8 @@ app.get("/",(req,res)=>{
     try{
         connection.query(q,(err,result)=>{
             if(err) throw err;
-            console.log(result[0]['COUNT(*)']);
-            res.render("home.ejs");
+            let count=result[0]['COUNT(*)'];
+            res.render("home.ejs",{count});
         });
     }
     catch(err){
@@ -47,7 +50,6 @@ app.get("/users",(req,res)=>{
     try{
         connection.query(q,(err,users)=>{
             if(err) throw err;
-            console.log(users);
             res.render("show.ejs",{users});
         });
     } catch(err){
@@ -69,6 +71,31 @@ app.get("/users/:id/edit",(req,res)=>{
     }
 })
 
+app.patch("/users/:id",(req,res)=>{
+    let {id} =req.params;
+    let{password:formpass, name:newname} =req.body;
+    let q=`SELECT * FROM user WHERE id='${id}'`;
+
+    try{
+        connection.query(q,(err,results)=>{
+            if(err) throw err;
+            let user=results[0];
+            if(formpass != user.password){
+                res.send("WRONG");
+            } else{
+                let q2=`UPDATE user SET name='${newname}' WHERE id='${id}'`;
+                connection.query(q2,(err,results)=>{
+                    if(err) throw err;
+                    res.render("/users");
+                })
+            }
+            
+        });
+    } catch(err){
+        console.log("Some error in DB");
+    }
+
+})
 app.listen(port,(req,res)=>{
     console.log(`Server is running on port ${port}`);
 })
